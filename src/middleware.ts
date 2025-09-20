f7c467b32439c433b225c80f6b7d3b61ba061100
jamirkhan6 <ajzomirkhan6@gmail.com>
2025-09-20 22:45:26 +0600

    middleware problem solve

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,27 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-
-    const {pathname} = req.nextUrl;
-
+  const { pathname } = req.nextUrl;
   const loggedIn = req.cookies.get("loggedIn")?.value === "true";
 
-  if (!loggedIn) {
-    return NextResponse.redirect(new URL("/SignIn", req.url))
+  // যদি logged in না থাকে এবং private route এ ঢুকতে চায়
+  if (!loggedIn && pathname.startsWith("/home")) {
+    return NextResponse.redirect(new URL("/SignIn", req.url));
   }
 
-  if (loggedIn && pathname.startsWith("/SignIn")) {
-    return NextResponse.redirect(new URL("/home", req.url))
-  }
-
-  
-  if (loggedIn && pathname.startsWith("/SignUp")) {
+  // যদি logged in থাকে এবং public route এ ঢুকতে চায়
+  if (
+    loggedIn &&
+    (pathname === "/" ||
+      pathname.startsWith("/SignIn") ||
+      pathname.startsWith("/SignUp"))
+  ) {
     return NextResponse.redirect(new URL("/home", req.url));
   }
-
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/home",],
+  matcher: ["/((?!_next|api).*)"], // সব route cover করবে
 };
